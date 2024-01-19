750caebb52903b8ee525806ee8f2a5ed7cc72ab8
Kornull <u.karnialiuk@gmail.com>
2024-01-19 19:03:27 +0300

    feat: add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NavigateComponent } from './navigate/navigate.component';
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavigateComponent],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
   ],
